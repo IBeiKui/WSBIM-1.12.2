c38a891180b07ad5ae4abedd8db44be45e34cc98
FinalKG <packapunch-9175@hotmail.com>
2020-01-28 22:41:20 -0600

    Add files via upload

--- com/finalkg/wsbim/common/lib/IColoredItem.java
+++ com/finalkg/wsbim/common/lib/IColoredItem.java
@@ -9,11 +9,11 @@ public interface IColoredItem{
 	/**Gets the default color of this chest item, USE ColorHelper.WHITE if your item will not have a default color*/
 	public abstract int getDefaultColor();
 	/**Gets the color for the itemstack. Return getDefaultColor() or -1 if it does not have a color*/
-	public abstract int getColor(ItemStack colorStack);
+	public abstract int getItemColor(ItemStack colorStack);
 	/**Sets the color for the chest itemstack*/
-	public abstract void setColor(ItemStack colorStack, int color);
+	public abstract void setItemColor(ItemStack colorStack, int color);
 	/**Remove the color for the chest itemstack*/
-	public abstract void removeColor(ItemStack colorStack);
+	public abstract void removeItemColor(ItemStack colorStack);
 	/**Returns true if the colored itemstack has a color. Use NBT parsing for all of these methods. See ItemArmor for examples.*/
-	public abstract boolean hasColor(ItemStack colorStack);
+	public abstract boolean hasItemColor(ItemStack colorStack);
 }
